ca484113661fc1169da63c3e22a3a130f639a807
macsor-hue <santosjohnmarco.basc@gmail.com>
2026-04-26 19:55:14 +0800

    Add files via upload

--- clientproject/includes/footer.php
+++ clientproject/includes/footer.php
@@ -2,8 +2,13 @@
     <div class="container">
         <div class="footer_content">
             <h2>Contact Us</h2>
-            <img src="resources/style/socials/facebook_icon.png" alt="Facebook Icon" class="social_fb">
+            <div class="socials">
+                 <img src="resources/style/socials/facebook_icon.png" alt="Facebook Icon" class="social_fb">
+            <a href="https://www.facebook.com/profile.php?id=61577942950184" target="_blank">Polished By Cha</a>
             <img src="resources/style/socials/phone.png" alt="Phone Icon" class="social_phone">
+            <h3>012-345-6789</h3>
+            </div>
+           
         </div>
         <div class="footer_content"></div>
         <div class="footer_content"></div>
--- clientproject/index.php
+++ clientproject/index.php
@@ -22,17 +22,30 @@
     </section>
     <section id="services" class="services">
         <div class="services_content">
-            <h1>We offer this bullshit</h1>
+            <h1 class="services_title">SERVICES</h1>
+        <div class="img_cont">
+            <div class="service_img1">
+                <h1>Service 1</h1>
+            </div>
+            <div class="service_img2">
+                <h1>Service 2</h1>
+            </div>
+            <div class="service_img3">
+                <h1>Service 3</h1>
+            </div>
+        </div>
         </div>
     </section>
     <section id="contact" class="contact">
-        <div class="contact_content">
-            <h1>Contact Us</h1>
+        <div class="contact_cont">
+            <div class="contact_content">
+                <h1 class="contact_title">Contact Us</h1>
+            </div>
         </div>
     </section>
     <section id="about" class="about">
         <div class="about_content">
-            <h1>About Us</h1>
+            <h1 class="about_title">About Us</h1>
         </div>
     </section>
     <?php include 'includes/footer.php'; ?>
